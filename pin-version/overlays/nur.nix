96b4ddaf7920f511eadd344619d3326d54e1cf1c
c4710n <c4710n@users.noreply.github.com>
2022-02-12 13:12:02 +0800

    fix: fix broken overlay - nur

--- pin-version/overlays/nur.nix
+++ pin-version/overlays/nur.nix
@@ -2,6 +2,6 @@ let
   sources = import ../../nix/sources.nix;
   repo = sources.NUR;
 in
-(self: super: {
-  nur = import repo { inherit super; };
+(self: pkgs: {
+  nur = import repo { inherit pkgs; };
 })
