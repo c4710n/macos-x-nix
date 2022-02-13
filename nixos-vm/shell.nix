469ab313b01de33e57fb2b3b441b499e222be993
c4710n <c4710n@users.noreply.github.com>
2022-02-13 22:44:18 +0800

    feat: manage virtual machine in this repo, too

---
+++ nixos-vm/shell.nix
@@ -0,0 +1,38 @@
+{ system ? builtins.currentSystem }:
+
+let
+  sources = import ../nix/sources.nix;
+  pkgs = import sources.nixos { inherit system; };
+
+  sshPrivateKey = ../secrets/nixos-vm-ssh-key-private;
+in
+pkgs.mkShell {
+  # I prefer overriding NIX_PATH rather than using `nix.nixPath`:
+  # + https://discourse.nixos.org/t/nixops-pinning-nixpkgs/734/3
+  # + https://github.com/NixOS/nixops/issues/736#issuecomment-333658173
+  NIX_PATH = "nixpkgs=${toString pkgs.path}";
+
+  # force Morph to use specified SSH private key in this repo, instead of keys
+  # in SSH agent. SSH_CONFIG_FILE is an env supported by Morph.
+  SSH_CONFIG_FILE = pkgs.writeText "ssh-config" ''
+    Host *
+      IdentityFile ${sshPrivateKey}
+      IdentitiesOnly yes
+  '';
+
+  # force SSH to use specified SSH private key in this repo, instead of keys
+  # in SSH agent.
+  buildInputs = [
+    (pkgs.writeScriptBin "vm-ssh" ''
+      #!${pkgs.stdenv.shell}
+
+      ${pkgs.openssh}/bin/ssh -i ${sshPrivateKey} $@
+    '')
+
+    (pkgs.writeScriptBin "vm-deploy" ''
+      #!${pkgs.stdenv.shell}
+
+      morph deploy ./network.nix switch
+    '')
+  ];
+}
