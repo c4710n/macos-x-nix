59b3e614388549ade905ac2b804378031446b5c4
c4710n <c4710n@users.noreply.github.com>
2022-03-25 12:53:56 +0800

    feat(dev-web): use new color picker - pika

--- modules/dev-web/default.nix
+++ modules/dev-web/default.nix
@@ -11,9 +11,7 @@
 
   homebrew.casks = [
     # color picker
-    "menubar-colors"
-    "colorpicker-propicker"
-    "colorpicker-skalacolor"
+    "pika"
 
     # better browser for developing web page
     # "sizzy" - currently, sizzy's download link is broken.
