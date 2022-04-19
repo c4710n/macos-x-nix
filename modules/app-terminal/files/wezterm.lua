7caaca9d2a4f41598741d057530fb511b389afee
c4710n <c4710n@users.noreply.github.com>
2022-04-19 11:28:00 +0800

    feat(app-terminal): use Modus themes

--- modules/app-terminal/files/wezterm.lua
+++ modules/app-terminal/files/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require 'wezterm';
 
 function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
-    return "Solarized Dark Higher Contrast"
+    return "Modus Vivendi"
   else
-    return "Builtin Solarized Light"
+    return "Modus Operandi"
   end
 end
 
@@ -18,10 +18,40 @@ wezterm.on("window-config-reloaded", function(window, pane)
   end
 end)
 
+function color_schemes()
+   return {
+      ["Modus Operandi"] = {
+         background = "#ffffff",
+         foreground = "#000000",
+         cursor_bg = "#000000",
+         cursor_fg = "#ffffff",
+         cursor_border = "#000000",
+         selection_fg = "#ffffff",
+         selection_bg = "#813e00",
+         ansi = {"#ffffff", "#a60000", "#005e00", "#813e00", "#0031a9", "#721045", "#00538b", "#000000"},
+         brights = {"#5C6370", "#a60000", "#005e00", "#813e00", "#0031a9", "#721045", "#00538b", "#000000"},
+      },
+
+      ["Modus Vivendi"] = {
+         background = "#000000",
+         foreground = "#ffffff",
+         cursor_bg = "#ffffff",
+         cursor_fg = "#000000",
+         cursor_border = "#ffffff",
+         selection_fg = "#000000",
+         selection_bg = "#d0bc00",
+         ansi = {"#000000", "#ff8059", "#44bc44", "#d0bc00", "#2fafff", "#feacd0", "#00d3d0", "#ffffff"},
+         brights = { "#5c6370", "#ff8059", "#44bc44", "#d0bc00", "#2fafff", "#feacd0", "#00d3d0", "#ffffff"},
+      }
+   }
+end
+
 return {
    font = wezterm.font("PragmataPro Mono"),
    font_size = 15.0,
    freetype_load_flags = "NO_HINTING",
+
+   color_schemes = color_schemes(),
 
    default_prog = {"/run/current-system/sw/bin/tmux", "new-session", "-A", "-s", "main"},
 
