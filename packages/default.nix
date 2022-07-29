42d55aeafe1a0be0971ee3567663e79593914e29
c4710n <c4710n@users.noreply.github.com>
2022-07-29 11:42:22 +0800

    feat(packages): remove trojan

--- packages/default.nix
+++ packages/default.nix
@@ -14,9 +14,10 @@ in
 
       custom = {
         mg = pkgs.callPackage ./mg { };
-        trojan = pkgs.callPackage ./trojan { };
+
         sarasa-mono-sc-nerd-font = pkgs.callPackage ./sarasa-mono-sc-nerd-font { };
         pragmata-pro-font = pkgs.callPackage ./pragmata-pro-font { };
+
         marmaduke-chromium = pkgs.callPackage ./marmaduke-chromium { };
         thorium-chromium = pkgs.callPackage ./thorium-chromium { };
         macos-librewolf = pkgs.callPackage ./macos-librewolf { };
